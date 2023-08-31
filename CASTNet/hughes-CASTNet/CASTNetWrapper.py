a9478dbaf81e4d6389832d9dc3bcc37a8f7f8cac
Jyontika Kapoor <jk103@wellesley.edu>
2023-08-31 11:39:06 -0400

    Update CASTNetWrapper.py

--- CASTNet/hughes-CASTNet/CASTNetWrapper.py
+++ CASTNet/hughes-CASTNet/CASTNetWrapper.py
@@ -6,15 +6,18 @@ import tensorflow as tf
 import pickle
 import hughes_castnet_model
 
+#create a wrapper around CASTNet. Lets users train, predict, and save CASTNet
+#dataset 
 class CASTNetWrapper:
 
     def __init__(self, conf):
         """
-        Initializes the CASTNetWrapper class.
+        Initializes the CASTNetWrapper object
         """
         self.conf = conf
         self.sess = tf.compat.v1.Session()  
         self.castnet_model = hughes_castnet_model.CASTNet(self.sess, conf)  # initialize CASTNet model
+
     
     def train(self, train_svi_local, train_svi_global, train_static, train_sample_indices, train_dist, train_y,
               valid_svi_local, valid_svi_global, valid_static, valid_sample_indices, valid_dist, valid_y,
@@ -36,21 +39,19 @@ class CASTNetWrapper:
         return self.castnet_model.predict(svi_data_local, svi_data_global, static_data, sample_indices, dist_data)
     
     def save(self, save_path):
+        """Saves CASTNet model to file"""
         self.castnet_model.save(save_path)
     
     def load(self, load_path):
         self.castnet_model.load(load_path)
     
-    def save_results(cook, test_times):
+    def save_results(dataset_name, test_times):
         """
-        @param cook (bool)
+        @param dataset_name (string), "Chicago" for cook, "MA" for MA
         @param test_times would be [1,2] for 2 test times"""
         data_dir = os.environ.get('DATA_DIR', '/Users/jyontika/Desktop/opioid-overdose-models/CASTNet/hughes-CASTNet/')
         
-        if cook:
-            results_folder = 'Chicago'
-        else:
-            results_folder = 'MA'
+        results_folder = dataset_name
 
         results = []
         for test_time in test_times:
@@ -74,7 +75,7 @@ class CASTNetWrapper:
 
         df_predictions = []
         for idx, prediction_lead in enumerate(prediction_leads):
-            year = str(2021 + test_times[idx]) if cook else str(2019 + test_times[idx])
+            year = str(2021 + test_times[idx]) if dataset_name=='Chicago' else str(2019 + test_times[idx])
             df_prediction = pd.DataFrame({
                 'geoid': locations,
                 'prediction': prediction_lead,
@@ -84,30 +85,27 @@ class CASTNetWrapper:
 
         combined_df = pd.concat(df_predictions)
 
-        csv_filename = f"Results/{'cook-county' if cook else 'MA'}-predictions.csv"
+        csv_filename = f"Results/{'cook-county' if dataset_name=='Chicago' else 'MA'}-predictions.csv"
         csv_filepath = os.path.join(data_dir, csv_filename)
 
         combined_df.to_csv(csv_filepath, index=False)
 
 
         
-    def load_results_and_locations(self, Cook):
+    def load_results_and_locations(self, dataset_name):
         """
         Loads CASTNet results and location information.
 
-        @param Cook (bool): Whether to load results for Cook County (True) or MA (False).
+        @param dataset_name (string): set dataset_name to 'cook-county' or 'MA.
 
         Returns:
             CN_results: df containing CASTNet results.
             CN_locations: list of location information.
         """
         data_dir = os.environ.get('DATA_DIR', '/Users/jyontika/Desktop/opioid-overdose-models/CASTNet/hughes-CASTNet/')
-        if Cook:
-            results_path = os.path.join(data_dir, 'Results/cook-county-predictions.csv') 
-            locations_path = os.path.join(data_dir, 'Data/Chicago/locations.txt')
-        else:
-            results_path = os.path.join(data_dir, 'Results/MA-predictions.csv') 
-            locations_path = os.path.join(data_dir, 'Data/MA/locations.txt')
+        results_path = os.path.join(data_dir, 'Results/' , dataset_name, '-predictions.csv') 
+        locations_path = os.path.join(data_dir, 'Data/' , dataset_name, '/locations.txt')
+
         
         CN_results = pd.read_csv(results_path)
         CN_results['geoid'] = CN_results['geoid'].astype(str)
